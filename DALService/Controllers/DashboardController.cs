2f427cb8cef852c93a7840d172bff04c3ba732ff
Viktor <Viktor@VIKTOR-PC>
2017-03-12 20:03:49 +0200

    dashboard

---
+++ DALService/Controllers/DashboardController.cs
@@ -0,0 +1,31 @@
+﻿using LocalApplicationServices;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace DALService.Controllers
+{
+    public class DashboardController : BaseApiController
+    {
+        public readonly IDashboardManager dashboardInfoProvider;
+
+        public DashboardController(IDashboardManager dashboardInfoProvider)
+        {
+            this.dashboardInfoProvider = dashboardInfoProvider;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetSuitiblePositions()
+        {
+            return ExecuteAction(() =>
+            {
+                var queryString = Request.GetQueryNameValuePairs();
+                var sectorId = queryString.Where(x => x.Key == "sectorId").FirstOrDefault().Value;
+                var countryId = queryString.Where(x => x.Key == "countryId").FirstOrDefault().Value;
+                var userId = queryString.Where(x => x.Key == "userId").FirstOrDefault().Value;
+
+                return dashboardInfoProvider.GetSuitiblePositions(sectorId, countryId, userId);
+            });
+        }
+    }
+}
---
+++ LocalApplicationServices/ProfileManagement/Managers/DashboardManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+using Data.Unit_Of_Work;
+using Executors;
+using LocalApplicationServices.ProfileManagement.Contracts;
+
+namespace LocalApplicationServices
+{
+    public class DashboardManager : Base, IDashboardManager
+    {
+        public DashboardManager(IDALServiceData dalServiceData) : base(dalServiceData) { }
+
+        public IList<UserSuitiblePosition> GetSuitiblePositions(string sectorIdQuery, string countryIdQuery, string userId)
+        {
+            int? sectorId = !string.IsNullOrWhiteSpace(sectorIdQuery) ? int.Parse(sectorIdQuery) : (int?)null;
+            int? countryId = !string.IsNullOrWhiteSpace(countryIdQuery) ? int.Parse(countryIdQuery) : (int?)null;
+
+            var user = dalServiceData.Users.FindEntity(x => x.Email == userId);
+
+            var matchedPositions = new UserMatchingExecutor(dalServiceData).Match(user, sectorId, countryId);
+
+            return matchedPositions;
+        }
+    }
+}
